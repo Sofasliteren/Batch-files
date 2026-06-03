50f102c3c5cd798232e8a93cdae9666f79b11766
Markus Westermoen <123084064+Sofasliteren@users.noreply.github.com>
2026-06-03 12:49:36 +0200

    Update ChangeIP.bat
    Fixed the gateway issue using "setlocal EnableDelayedExpansion"
     and "for /f "tokens=1-4 delims=." %%a in ("!ip!") do ("

--- ChangeIP.bat
+++ ChangeIP.bat
@@ -1,13 +1,17 @@
 @echo off
 REM ****Simple bat to set IP without going through the Windows-menues ****
 
+setlocal EnableDelayedExpansion
+
 IF "%1"=="" (
 Echo Add an IP behind the ChangeIP or set 0 to get initial IP. Run as admininstrator for this to work!
 exit /b
 )
 
 IF "%1"=="0" (
-netsh interface ipv4 set address name="Ethernet" static 192.168.30.219 255.255.255.0 192.168.30.1
+REM netsh interface ipv4 set address name="Ethernet" static 192.168.30.219 255.255.255.0 192.168.30.1
+set "ip=192.168.30.219"
+set "gateway=192.168.30.1"
 echo Resetting IP on Ethernet to 192.168.30.219 gateway 192.168.30.1
 ) ELSE (
 
@@ -16,7 +20,7 @@ REM Validate IP
 REM -------------------------
 set ip=%~1
 
-for /f "tokens=1-4 delims=." %%a in ("%ip%") do (
+for /f "tokens=1-4 delims=." %%a in ("!ip!") do (
     set o1=%%a
     set o2=%%b
     set o3=%%c
@@ -27,15 +31,14 @@ REM Ensure all 4 octets exist
 if "!o4!"=="" (
     echo Invalid IP format.
     exit /b 1
+ )
+
+set "gateway=!o1!.!o2!.!o3!.1"
 )
 
-REM Set the gateway and apply the IP and gateway with a fixed mesh
+REM set gateway=%o1%.%o2%.%o3%.1
 
-set gateway=%o1%.%o2%.%o3%.1
-netsh interface ipv4 set address name="Ethernet" static %1 255.255.255.0 %gateway%
-echo Finished: IP on Ethernet set to %1 with gateway %gateway%
-
-
-)
+netsh interface ipv4 set address name="Ethernet" static !ip! 255.255.255.0 !gateway!
+echo Finished: IP on Ethernet set to %1 with gateway !gateway!
 
 pause
